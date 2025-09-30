75ea24ffda362af86f808679d4dc9ac4dd6007d5
Shubham <47573854+ssagar1999@users.noreply.github.com>
2025-09-30 13:28:47 +0530

    Create closureExample.js

---
+++ closureExample.js
@@ -0,0 +1,18 @@
+function getEmployeeFactory() {
+  let employeeNumber = 1;
+  return function(name, country) {
+    return { employeeNumber: employeeNumber++, name, country };
+  };
+}
+
+const getEmployee = getEmployeeFactory();
+
+const employeeOne = getEmployee('Robin', 'Germany');
+// employeeNumber = 50; NOT POSSIBLE WITH CLOSURE
+const employeeTwo = getEmployee('Markus', 'Canada');
+
+const employees = [employeeOne, employeeTwo];
+
+console.log(employees);
+
+// guess the output
